5e02e26a59ae90cb23950dda064d14410d477c24
Eryn Cangi <emcangi@gmail.com>
2016-07-20 16:48:51 -0600

    EOD July 20 - fixed timegcm_analysis_bylat to make plots too and some other stuff. restored timegcm_analysis just in case.

--- timegcm_analysis.py
+++ timegcm_analysis.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
-ANALYZE TIME-GCM DATA
+Analyzes TIME-GCM data for a particular latitude. Allows creation of plots by
+longitude of tidal values.
 
 Author: Eryn Cangi
 LASP REU, CU Boulder
@@ -10,33 +11,38 @@ LASP REU, CU Boulder
 from lunar_tide_extraction import *
 
 fname = 'Analysis/TIME-GCM/TUVZ_allLev_timegcm_wg5_zmPlev05_GPI_lunarM2_tmp4.nc'
-lat = -45
-type = 'temp'
-
-# Extract and format the data from the NetCDF file
-timegcm_data = format_timegcm_data(fname, lat, 'TN')
-print('Finished formatting data')
 
 # =========================== PARAMETERS & VARIABLES ===========================
 # You may change these variables
-start = timegcm_data[0, 3]
-duration = [15, 23, 30]
-fr = 'reconM2_{}_lat={}_timegcm_{}.txt'       # File to write reconstructed data
-result_file = 'timegcm_results_{}.txt'        # Stores results
-L = 150                                  # Longitude to use for plotting results
-cycle = ['Half', '75%', 'Full']
+duration = [15, 23, 30]                       # Part of lunar cycle to use
+cycle = ['Half', '75%', 'Full']               # For plotting purposes
+f1 = 'reconM2_{}_lat={}_timegcm_{}.txt'       # File to write reconstructed data
+f2 = 'timegcm_lat={}_results_{}.txt'          # Stores found amplitude & phase
+L = 150                                       # Longitude to use for plotting
+lat = -45
+tide_type = 'temp'                                 # 'temp' or 'zonal wind'
 
 # Strongly suggested not to change these variables
-binsz = 1                     # Bin size to use when doing SLT and LLT binning
+BINSZ = 1                     # Bin size to use when doing SLT and LLT binning
 N = [2]                       # Values of v to use in format [n1, n2...]
 S = [2]                       # Values of s to use in format [s1, s2...]
 
+if tide_type == 'temp':
+    v = 'TN'
+elif tide_type == 'zonal wind':
+    v = 'UN'
+else:
+    raise ValueError("Type must be specified as either 'temp' or 'zonal wind'!")
 
-# =================================== MAIN =====================================
+
+# ================================== MAIN ======================================
+# Extract and format the data from the NetCDF file
+timegcm_data = format_timegcm_data(fname, lat, v)
+start = timegcm_data[0, 3]
 
 # Initialize results file
-f = open(result_file.format(type), 'w')
-f.write('TIME-GCM Analysis, {}, lat = {}\n'.format(type, lat))
+f = open(f2.format(lat, tide_type), 'w')
+f.write('TIME-GCM Analysis, {}, lat = {}\n'.format(tide_type, lat))
 f.close()
 
 for dur, cyc in zip(duration, cycle):
@@ -44,23 +50,19 @@ for dur, cyc in zip(duration, cycle):
     # Extract only entries that are within the cycle length specified by dur
     data = timegcm_data[np.where(timegcm_data[:, 3] <= start + dur)]
 
-    # Bin generated data by solar local time
-    means_slt = bin_by_solar(data, binsz)
-
-    # Subtract the means by SLT from original data
-    nosol = remove_solar(data, means_slt, binsz)
-
-    # Bin the results by lunar local time
-    recon_M2_llt_bin = bin_by_lunar(nosol, binsz)
+    # Bin data by SLT, subtract SLT average and bin residuals by LLT
+    means_slt = bin_by_solar(data, BINSZ)
+    nosol = remove_solar(data, means_slt, BINSZ)
+    recon_M2_llt_bin = bin_by_lunar(nosol, BINSZ)
 
     # Write the results of lunar binning reconstructed M2 to a file
-    cells = '{:<20}\t'*3
-    line0 = cells.format('Lunar local time', 'Longitude',
-                         'Lunar Tide (m/s)')
-    np.savetxt(fr.format(cyc, lat, type), recon_M2_llt_bin, fmt='%-20.4f',
-               delimiter='\t', header=line0, comments='')
+    # cells = '{:<20}\t'*3
+    # line0 = cells.format('Lunar local time', 'Longitude',
+    #                      'Lunar Tide (m/s)')
+    # np.savetxt(fr.format(cyc, lat, type), recon_M2_llt_bin, fmt='%-20.4f',
+    #            delimiter='\t', header=line0, comments='')
 
-    # SCIPY CURVE_FIT --------------------------------------------------
+    # SCIPY CURVE_FIT ----------------------------------------------------------
     guess_mean = np.mean(recon_M2_llt_bin[:, 2])
     guess_amp = np.max(recon_M2_llt_bin[:, 2]) - guess_mean
     guess_phase = 0
@@ -68,11 +70,11 @@ for dur, cyc in zip(duration, cycle):
     guess = [guess_amp, guess_phase, guess_mean]       # Initial parameter guess
     ap = amp_and_phase(recon_M2_llt_bin, guess, N[0], S[0])
 
-    # WRITE RESULTS ----------------------------------------------------
+    # WRITE RESULTS ------------------------------------------------------------
 
-    with open(result_file.format(type), 'a') as f:
+    with open(f2.format(lat, tide_type), 'a') as f:
         f.write('{} lunar cycle\n'.format(cyc))
-        f.write('Bin size: {} hr\n'.format(binsz))
+        f.write('Bin size: {} hr\n'.format(BINSZ))
         f.write('Reconstructed lunar amplitude: {}\n'.format(round(ap[0], 2)))
         f.write('Reconstructed lunar phase: {}\n'.format(round(ap[1], 2)))
         f.write('Reconstructed baseline: {}\n'.format(round(ap[2], 2)))
@@ -85,7 +87,7 @@ for dur, cyc in zip(duration, cycle):
 
     # Generate data to plot the fit line
     fit = ap[0] * np.cos((2 * pi * N[0] / 24) * recon_L[:, 0] +
-                             (S[0] - N[0]) * L - ap[1]) + ap[2]
+                         (S[0] - N[0]) * L - ap[1]) + ap[2]
 
     plt.figure(figsize=(10, 8))
 
@@ -103,5 +105,4 @@ for dur, cyc in zip(duration, cycle):
     plt.tight_layout()
     fn = title + '.png'
     plt.savefig(fn, bbox_inches='tight')
-    #plt.show()
-
+    plt.show()
--- timegcm_analysis_bylat.py
+++ timegcm_analysis_bylat.py
@@ -9,18 +9,24 @@ LASP REU, CU Boulder
 
 from lunar_tide_extraction import *
 
-fname = 'Analysis/TIME-GCM/TUVZ_allLev_timegcm_wg5_zmPlev05_GPI_lunarM2_tmp4.nc'
-type = 'zonal wind'
-
 # =========================== PARAMETERS & VARIABLES ===========================
 # You may change these variables
 duration = 14.5                                    # Cycle to use = 14.5 days
 result_file = 'timegcm_results_{}.txt'             # Stores results
+tide_type = 'zonal wind'
 
 # Strongly suggested not to change these variables
 BINSZ = 1                                     # Bin size for SLT and LLT binning
 N = [2]                                       # Values of n, format [n1, n2...]
 S = [2]                                       # Values of s, format [s1, s2...]
+fname = 'Analysis/TIME-GCM/TUVZ_allLev_timegcm_wg5_zmPlev05_GPI_lunarM2_tmp4.nc'
+
+if tide_type == 'temp':
+    v = 'TN'
+elif tide_type == 'zonal wind':
+    v = 'UN'
+else:
+    raise ValueError("Type must be specified as either 'temp' or 'zonal wind'!")
 
 
 # =================================== MAIN =====================================
@@ -30,7 +36,7 @@ results = []          # normally bad form, but okay because data isn't that long
 for lat in range(-90, 90, 5):
 
     # Extract and format the data from the NetCDF file
-    timegcm_data, real_lat = format_timegcm_data(fname, lat, 'UN')
+    timegcm_data, real_lat = format_timegcm_data(fname, lat, v)
     LAST_DATE = timegcm_data[-1, 3]  # Ending date in file
 
     # Set up sliding window
@@ -40,9 +46,9 @@ for lat in range(-90, 90, 5):
 
     while window_end <= LAST_DATE:
         # Extract only entries that are within the current window
-        c = (window_start <= timegcm_data[:, 3]) & (timegcm_data[:,
-                                                    3] <= window_end)
-        data = timegcm_data[np.where(c)]
+        tide = (window_start <= timegcm_data[:, 3]) & (timegcm_data[:,
+                                                       3] <= window_end)
+        data = timegcm_data[np.where(tide)]
 
         # Bin by SLT, subtract SLT average from original data, then bin by LLT
         means_slt = bin_by_solar(data, BINSZ)
@@ -51,11 +57,13 @@ for lat in range(-90, 90, 5):
 
         # SCIPY CURVE_FIT ------------------------------------------------------
         guess_mean = np.mean(recon_M2_llt_bin[:, 2])
-        guess_amp = np.max(recon_M2_llt_bin[:, 2]) - guess_mean
+        max_A = np.max(recon_M2_llt_bin[:, 2])
+        guess_amp = max_A - guess_mean
         guess_phase = 0
-
         guess = [guess_amp, guess_phase, guess_mean]   # Initial parameter guess
-        ap = amp_and_phase(recon_M2_llt_bin, guess, N[0], S[0])
+
+        bounds = [[0, -2*pi, -np.inf], [10000, 2*pi, np.inf]]
+        ap = amp_and_phase(recon_M2_llt_bin, guess, N[0], S[0], bounds=bounds)
 
         # WRITE RESULTS --------------------------------------------------------
 
@@ -63,16 +71,117 @@ for lat in range(-90, 90, 5):
         window_start += 1
         window_end += 1
         window_ctr += 1
+
     print('{} complete'.format(lat))
 
 results = np.asarray(results)
 cells = '{:<24}'*4
 line0 = cells + '\n\n{:<24}' + cells
-line0 = line0.format('TIME-GCM Analysis', type, 'Half lunar cycle, 14.5 days',
-                     'Bin size: {} hr'.format(BINSZ), 'Amplitude', 'Phase',
-                     'Vert offset', 'Date', 'Lat')
+line0 = line0.format('TIME-GCM Analysis', tide_type, 'Half lunar cycle, '
+                     '14.5 days', 'Bin size: {} hr'.format(BINSZ),
+                     'Amplitude', 'Phase', 'Vert offset', 'Date', 'Lat')
 
-np.savetxt(result_file.format(type), results, fmt='%-20.4f',
-                       delimiter='\t', header=line0, comments='')
+np.savetxt(result_file.format(tide_type), results, fmt='%-20.4f',
+           delimiter='\t', header=line0, comments='')
 
+# ============= MANIPULATE DATA TO PREPARE FOR PLOTTING ========================
+# Create new array of data in a logical format
+amp_data = np.column_stack((results[:, 3], results[:, 4], results[:, 0]))
+phase_data = np.column_stack((results[:, 3], results[:, 4], results[:, 1]))
 
+# Change numbers so they function as indices
+temp = np.copy(amp_data)
+temp[:, 0] = temp[:, 0] - 2456300.7917
+temp[:, 1] = temp[:, 1] + 88.75
+
+temp2 = np.copy(phase_data)
+temp2[:, 0] = temp2[:, 0] - 2456300.7917
+temp2[:, 1] = temp2[:, 1] + 88.75
+
+amps = np.zeros([int(max(temp[:, 1]) + 1), int(max(temp[:, 0]) + 1)])
+phases = np.zeros([int(max(temp[:, 1]) + 1), int(max(temp[:, 0]) + 1)])
+
+# Fill the array for plotting with values according to location - this fills
+# it upside down because of the way arrays vs plots are indexed
+for a, p in zip(temp, temp2):
+    x = a[0]
+    y = a[1]
+    z1 = a[2]
+    z2 = p[2]
+    amps[y, x] = z1
+    phases[y, x] = z2
+
+amps = amps[~np.all(amps == 0, axis=1)]  # Get rid of lines with all 0s
+phases = phases[~np.all(phases == 0, axis=1)]
+
+#=============================== MAKE PLOT =====================================
+
+# Make tick labels
+column_labels = np.asarray(sorted(set(amp_data[:, 0])))
+column_labels = np.around(column_labels - 2456293.5416666665, 2)
+row_labels = list(np.around(sorted(set(amp_data[:, 1])), 2))
+
+# Blank out most labels so they don't clutter things
+for i in range(len(row_labels)):
+    if i % 5 != 0:
+        row_labels[i] = ''
+
+# Plot a heat map
+fig = plt.figure(figsize=(16, 10))
+plt.axis([0, 41, 0, 36])  # this line makes sure the axes fit the fig!
+heatmap_A = plt.pcolor(amps, cmap=plt.cm.seismic)
+
+ax = fig.gca()
+ax.set_xticks(np.arange(0, amps.shape[1]))
+ax.set_yticks(np.arange(0, amps.shape[0]))
+
+ax.set_xticklabels(column_labels, rotation=45)
+ax.set_yticklabels(row_labels)
+plt.ylabel('Latitude')
+plt.xlabel('Days since 2013-01-01')
+
+plt.rcParams.update({'font.size': 16})
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=12)
+plt.title('M2 tidal amplitude (zonal wind speeds)', y=1.05)
+
+maxtide = round(np.max(amps))
+cbarticks = np.linspace(0, maxtide, 10)
+cbarticklabels = ['{} m/s'.format(round(i/100)) for i in cbarticks]
+cbar = fig.colorbar(heatmap_A, ticks=cbarticks)
+cbar.ax.set_yticklabels(cbarticklabels)
+
+plt.savefig('m2tidebylat_amp.png', facecolor='w', edgecolor='w',
+            format='png', transparent=False, bbox_inches='tight')
+
+plt.show()
+
+fig = plt.figure(figsize=(16, 10))
+plt.axis([0, 41, 0, 36])  # this line makes sure the axes fit the fig!
+heatmap_P = plt.pcolor(phases, cmap=plt.cm.seismic)
+
+ax = fig.gca()
+ax.set_xticks(np.arange(0, phases.shape[1]))
+ax.set_yticks(np.arange(0, phases.shape[0]))
+
+ax.set_xticklabels(column_labels, rotation=45)
+ax.set_yticklabels(row_labels)
+plt.ylabel('Latitude')
+plt.xlabel('Days since 2013-01-01')
+
+plt.rcParams.update({'font.size': 16})
+plt.rc('xtick', labelsize=8)
+plt.rc('ytick', labelsize=12)
+plt.title('M2 tidal phases (zonal wind speeds)', y=1.05)
+
+maxtide = round(np.max(phases))
+mintide = np.min(phases)
+cbarticks = np.linspace(mintide, maxtide, 10)
+cbarticklabels = ['{} m/s'.format(i) for i in cbarticks]
+cbar = fig.colorbar(heatmap_P, ticks=cbarticks)
+cbar.ax.set_yticklabels(cbarticklabels)
+
+plt.savefig('m2tidebylat_phase.png', facecolor='w', edgecolor='w',
+            format='png', transparent=False, bbox_inches='tight')
+
+plt.show()
